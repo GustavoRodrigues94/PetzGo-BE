57cb2cabfa95f66d3cd31251f7e3cb6c4c4cb544
Gustavo Rodrigues <gustavo.rodrigues@prks.com.br>
2021-12-06 23:34:07 -0300

    implementando testes nos comandos da empresa

---
+++ tests/PetzGo.Cadastros.Testes/Aplicacao.Testes/Comandos/EmpresaComandos/CriarEmpresaComandoTeste.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace PetzGo.Cadastros.Aplicacao.Testes.Comandos.EmpresaComandos
+{
+    [Collection(nameof(EmpresaComandosCollection))]
+    public class CriarEmpresaComandoTeste
+    {
+        private readonly EmpresaComandosTestesFixture _empresaComandosTestesFixture;
+
+        public CriarEmpresaComandoTeste(EmpresaComandosTestesFixture empresaComandosTestesFixture)
+        {
+            _empresaComandosTestesFixture = empresaComandosTestesFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar empresa comando válido")]
+        [Trait("Categoria", "PetzGo.Cadastros.Aplicacao.Empresa")]
+        public void CriarEmpresaComando_DeveSerValido()
+        {
+            // Arrange
+            var criarEmpresaComando = _empresaComandosTestesFixture.GerarCriarEmpresaComandoValido();
+
+            // Act
+            criarEmpresaComando.Validar();
+
+            // Assert
+            Assert.True(criarEmpresaComando.Valid);
+            Assert.Equal(0, criarEmpresaComando.Notifications.Count);
+        }
+
+        [Fact(DisplayName = "Adicionar empresa comando inválido")]
+        [Trait("Categoria", "PetzGo.Cadastros.Aplicacao.Empresa")]
+        public void CriarEmpresaComando_DeveSerInvalido()
+        {
+            // Arrange
+            var criarEmpresaComando = _empresaComandosTestesFixture.GerarCriarEmpresaComandoInvalido();
+
+            // Act
+            criarEmpresaComando.Validar();
+
+            // Assert
+            Assert.True(criarEmpresaComando.Invalid);
+            Assert.NotEqual(0, criarEmpresaComando.Notifications.Count);
+        }
+    }
+}
---
+++ tests/PetzGo.Cadastros.Testes/Aplicacao.Testes/Comandos/EmpresaComandos/EmpresaComandosTestesFixture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using PetzGo.Cadastros.Aplicacao.Comandos.EmpresaComandos;
+using PetzGo.Cadastros.Aplicacao.Comandos.EmpresaComandos.DTOsComandos;
+using Xunit;
+
+namespace PetzGo.Cadastros.Aplicacao.Testes.Comandos.EmpresaComandos
+{
+    [CollectionDefinition(nameof(EmpresaComandosCollection))]
+    public class EmpresaComandosCollection : ICollectionFixture<EmpresaComandosTestesFixture>
+    { }
+
+    public class EmpresaComandosTestesFixture : IDisposable
+    {
+        public CriarEmpresaComando GerarCriarEmpresaComandoValido() => new CriarEmpresaComando(
+            new EmpresaComando
+            {
+                CNPJ = "21075365000155",
+                NomeFantasia = "Petz & Cia",
+                TipoNegocioId = Guid.NewGuid(),
+                WhatsApp = "67999419457"
+            }, 
+            new EnderecoEmpresaComando
+            {
+                CEP = "15085360",
+                Rua = "Rua Otávio Pinto Cesar",
+                Numero = "915",
+                Cidade = "São José do Rio Preto",
+                Estado = "SP",
+                Bairro = "Cidade Nova",
+                Complemento = "AP. 23"
+            }, 
+            new LoginComando
+            {
+                Email = "gustavorodriguestsi@gmail.com",
+                Senha = "123456"
+            });
+
+        public CriarEmpresaComando GerarCriarEmpresaComandoInvalido() => new CriarEmpresaComando(
+            new EmpresaComando
+            {
+                CNPJ = "35945730874",
+                NomeFantasia = "Petz & Cia",
+                TipoNegocioId = Guid.NewGuid(),
+                WhatsApp = "67999419457"
+            },
+            new EnderecoEmpresaComando
+            {
+                CEP = "15085",
+                Rua = "Rua Otávio Pinto Cesar",
+                Numero = "915",
+                Cidade = "São José do Rio Preto",
+                Estado = "SP",
+                Bairro = "Cidade Nova",
+                Complemento = "AP. 23"
+            },
+            new LoginComando
+            {
+                Email = "gustavorodriguestsi",
+                Senha = "123456"
+            });
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
